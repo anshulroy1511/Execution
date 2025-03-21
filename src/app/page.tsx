c1f127340f4cf8163135023890767f583b703273
HEMANT-YADAV123 <hy700793@gmail.com>
2025-03-21 10:12:42 +0530

    added chatbot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
+import ChatBot from "./ChatBot/page";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
-    <p>hii
-
-    </p>
+      <Head>
+        {/* Google Fonts for Material Icons */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0"
+        />
+      </Head>
+      <ChatBot />
     </div>
   );
 }
-
